7c5b1d22dda3c2926a1f4b4409db4fe82995afc3
ijcastrosena <111159778+ijcastrosena@users.noreply.github.com>
2022-09-05 20:18:55 -0500

    Detalle - Borrar - Actualizar

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,8 @@ public class HomeController : Controller
         return View(await _contexto.Contacto.ToListAsync());
     }
     
+    // Crear 
+
     [HttpGet]
     public IActionResult Crear() 
     {
@@ -34,11 +36,105 @@ public class HomeController : Controller
     {
         if (ModelState.IsValid)
         {
+            //  Metodo Add lo utilizamo para crear registros en la base de datos (INSERT INTO contacto VALUES ()
             _contexto.Contacto.Add(contacto);
             await _contexto.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
         return View();
+    }
+
+
+    // Editar
+
+    [HttpGet]
+    public IActionResult Editar(int? id) 
+    {
+        if(id == null)
+        {
+            return NotFound();
+        }
+
+        var contacto = _contexto.Contacto.Find(id); // SQL (SELECT * FROM Contacto WHERE id = 1)
+
+        if(contacto == null)
+        {
+            return NotFound();
+        }
+
+        return View(contacto);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Editar(Contacto contacto) //
+    {
+        if (ModelState.IsValid)
+        {
+            _contexto.Contacto.Update(contacto); 
+        
+            await _contexto.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View();
+    }
+
+    // Detalle
+    [HttpGet]
+    public IActionResult Detalle(int? id) 
+    {
+        if(id == null)
+        {
+            return NotFound();
+        }
+
+        var contacto = _contexto.Contacto.Find(id); // SQL (SELECT * FROM Contacto WHERE id = 1)
+
+        if(contacto == null)
+        {
+            return NotFound();
+        }
+
+        return View(contacto);
+    }
+
+    // Borrar
+    [HttpGet]
+    public IActionResult Borrar(int? id) 
+    {
+        if(id == null)
+        {
+            return NotFound();
+        }
+        var contacto = _contexto.Contacto.Find(id);
+
+        if(contacto == null)
+        {
+            return NotFound();
+        }
+
+        return View(contacto);
+
+    }
+
+
+    [HttpPost, ActionName("Borrar")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> BorrarContacto(int? id)
+    {
+        var contacto = await _contexto.Contacto.FindAsync(id);
+
+        if (contacto == null)
+        {
+            return View();
+        }
+
+        _contexto.Contacto.Remove(contacto); 
+        await _contexto.SaveChangesAsync();
+        
+        return RedirectToAction(nameof(Index));
+
     }
 
     public IActionResult Privacy()
